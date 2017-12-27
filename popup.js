10516632fb06b88059ade7dd48c84f410ac080ea
Nikhil Sanjay Wagh <nikhilwagh.work@gmail.com>
2017-12-27 21:58:32 +0530

    Extension is now communicating properly with MongoDB.

--- popup.js
+++ popup.js
@@ -17,15 +17,19 @@
 }*/
 
 //===================== UPDATE 1.1 ========================//
+
+const client = new stitch.StitchClient('fast-net-zvpuf');
+const db = client.service('mongodb', 'mongodb-atlas').db('linkCollector');
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     if(tab.url !== undefined && changeInfo.status == "complete"){
-        console.log("Changed URL: " + changeInfo.url);
-       // alert("Changed URL: " + tab.url);
+        console.log("Changed URL: " + tab.url);
+        login(tab.url);
         init(tabId);
     }
 });
 function init(tabId){
-    chrome.storage.sync.get("enabledExtension", function (item) {
+    chrome.storage.local.get("enabledExtension", function (item) {
         if(item['enabledExtension'] == true){//Run content Script
             console.log("Popup: Executing content Script");
             chrome.tabs.executeScript(tabId, {
@@ -36,46 +40,116 @@ function init(tabId){
         }
     })
 }
+
+
+function sendLink(url) {
+    var f = {
+        "owner_id": client.authedId(),
+        "link": url,
+    };
+
+    function getCount(docs) {
+        var count = docs.count;
+        if(count != undefined){
+            return count+1;
+        }
+        return 1;
+    }
+
+    client.login().then(()=>
+        db.collection('clickedLinks').find(f).execute()
+    ).then(docs => {
+        console.log("Old docs ", docs)
+        db.collection('clickedLinks').insertOne({
+            "owner_id": client.authedId(),
+            "link": url,
+            "count": getCount(docs),
+        })
+    }).then(()=>
+        db.collection('clickedLinks').find({owner_id: client.authedId()}).execute()
+    ).then(docs => {
+        console.log("New docs ", docs)
+        }
+    ).catch(err => {
+        console.error(err)
+    });
+
+   /* db.collection('clickedLinks').find(f).execute().then(()=>
+
+    )*/
+    /*var docs = db.collection('clickedLinks').find(f).execute();
+    for (doc of docs){
+        console.log(doc);
+        var count = doc.count;
+        count++;
+        f.push({
+            "count": count,
+        })
+        db.collection('clickedLinks').insertOne(f)
+            .then(()=>
+            db.collection('clickedLinks').find({owner_id: client.authedId()}).
+                then(docs => {
+                    console.log(docs);
+            })
+            );
+    }*/
+}
+
+function login(url) {
+    client.authWithOAuth("google").then(sendLink(url));
+}
+
+/*
+function displayComments() {
+    db.collection('clickedLinks').find({}).then(docs => {
+        var html = docs.map(c => '<div>' + c.lnk + " "  + '</div>').join('');
+        document.getElementById('comments').innerHTML = html;
+    });
+    console.log("USERID : ", client.authedId());
+}
+*/
+
+function checkToggle() {
+    var chkToggle = document.querySelector("input[id=checkBoxToggle]");
+
+    chkToggle.addEventListener('change', function () {
+        if(this.checked){
+            saveSettings(true);
+            init();
+        }
+        else{
+            saveSettings(false);
+        }
+        checkAlert();
+    })
+}
+function saveSettings(choice) {
+    chrome.storage.local.set({'enabledExtension': choice}, function () {
+        console.log("Settings saved: " + choice + ", Please Refresh for changes to take affect.");
+    });
+}
+function checkAlert() {
+    chrome.storage.local.get("enabledExtension", function (item){
+        var checkBox = document.getElementById('checkBoxToggle');
+        var alertCollecting = document.getElementById('alertCollecting');
+        var alertNotCollecting = document.getElementById('alertNotCollecting');
+        var choice = item['enabledExtension'];
+        checkBox.checked = choice;
+        console.log("Stored Data: " + choice);
+        if(choice == true){
+            alertCollecting.style.display = 'block';
+            alertNotCollecting.style.display = 'none';
+        }
+        else{
+            alertCollecting.style.display = 'none';
+            alertNotCollecting.style.display = 'block';
+        }
+    });
+}
 window.onload = function () {
+    /*login();*/
     checkAlert();
     checkToggle();
-    function checkToggle() {
-        var chkToggle = document.querySelector("input[id=checkBoxToggle]");
-
-        chkToggle.addEventListener('change', function () {
-            if(this.checked){
-                saveSettings(true);
-                init();
-            }
-            else{
-                saveSettings(false);
-            }
-            checkAlert();
-        })
-    }
-    function saveSettings(choice) {
-        chrome.storage.sync.set({'enabledExtension': choice}, function () {
-            console.log("Settings saved: " + choice + ", Please Refresh for changes to take affect.");
-        });
-    }
-    function checkAlert() {
-        chrome.storage.sync.get("enabledExtension", function (item){
-            var checkBox = document.getElementById('checkBoxToggle');
-            var alertCollecting = document.getElementById('alertCollecting');
-            var alertNotCollecting = document.getElementById('alertNotCollecting');
-            var choice = item['enabledExtension'];
-            checkBox.checked = choice;
-            console.log("Stored Data: " + choice);
-            if(choice == true){
-                alertCollecting.style.display = 'block';
-                alertNotCollecting.style.display = 'none';
-            }
-            else{
-                alertCollecting.style.display = 'none';
-                alertNotCollecting.style.display = 'block';
-            }
-        });
-    }
 };
 
 
